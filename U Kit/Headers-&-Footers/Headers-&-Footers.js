ea2c7d5a8a961535c4c49e27ce0c9b1106fb818a
yanastrunina <yana.strunina.9831@mail.ru>
2021-01-09 19:44:35 +0500

    added page headers and footers

---
+++ U Kit/Headers-&-Footers/Headers-&-Footers.js
@@ -0,0 +1,11 @@
+import '../../fonts/fonts.scss';
+import '../../components/header/header.scss';
+import '../../components/logo/logo.scss'
+import '../../components/buttons/buttons.scss';
+import '../../components/header/header.js';
+import '../../components/footer/footer.scss';
+import '../../components/footer/footer.js';
+import '../../components/cutaway/cutaway.scss';
+
+
+import './Headers-&-Footers.scss';
