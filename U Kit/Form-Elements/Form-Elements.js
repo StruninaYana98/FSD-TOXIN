be31f1e5e12f9eb2403e5861e3f5a5de4a538b48
yanastrunina <yana.strunina.9831@mail.ru>
2021-01-09 19:45:03 +0500

    added page form elements

---
+++ U Kit/Form-Elements/Form-Elements.js
@@ -0,0 +1,43 @@
+import '../../fonts/fonts.scss';
+import './Form-Elements.scss'
+import '../../components/logo/logo.scss'
+import '../../components/mixins/text-field/text-field.scss'
+import '../../components/mixins/text-field/text-field.js'
+import '../../components/checkbox/checkbox-buttons/checkbox-buttons.scss'
+import '../../components/radio-buttons/radio-buttons.scss'
+import '../../components/buttons/buttons.scss'
+import '../../components/checkbox/rich-checkbox/rich-checkbox.scss'
+import '../../components/bullet-list/bullet-list.scss'
+
+
+import '../../components/rate-buttons/rate-buttons.js'
+import '../../components/rate-buttons/rate-buttons.scss'
+import '../../components/toggle/toggle.scss'
+import '../../components/like-buttons/like-buttons.scss'
+import '../../components/like-buttons/like-buttons.js'
+import '../../components/room-details/room-details.scss'
+import '../../components/comments/comments.scss'
+import '../../components/checkbox/expandable-checkbox/expandable-checkbox.scss'
+import '../../components/checkbox/expandable-checkbox/expandable-checkbox.js'
+import '../../components/range-slider/range-slider.js'
+import '../../components/range-slider/range-slider.scss'
+
+import '../../components/calendar/calendar.js'
+import '../../components/calendar/calendar.scss'
+import '../../components/date-dropdown/date-dropdown.js'
+import '../../components/date-dropdown/date-dropdown.scss'
+import '../../components/filter-date-dropdown/filter-date-dropdown.js'
+import '../../components/filter-date-dropdown/filter-date-dropdown.scss'
+import '../../components/pagination/pagination.js'
+import '../../components/pagination/pagination.scss'
+
+import '../../components/dropdown/option/option.scss'
+import '../../components/dropdown/option/option.js'
+
+import '../../components/dropdown/room-dropdown/room-dropdown.scss'
+import '../../components/dropdown/room-dropdown/room-dropdown.js'
+
+import '../../components/dropdown/guests-dropdown/guests-dropdown.scss'
+import '../../components/dropdown/guests-dropdown/guests-dropdown.js'
+
+
